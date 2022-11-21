e99e82394e6b929a83c6bae2bd30f0d06c5890e9
Eblackk <aps14blacerig@gmail.com>
2022-11-21 17:21:01 -0500

    updated notes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+class Solution_BreakDown {
+    public Integer totalFruits(int[] fruits) {
+        //An Array is a container object that holds a fixed number of values of a single type
+        //Each item in an array is called an "ELEMENT"
+        // One array can have more than one "SUB-ARRAY" withIn
+        // Array Ex. [1,2,3,4]
+        //SUB-ARRAY EX. [1,2], [3,4], [1,2,3], and [2,3,4]
+
+
+        //Set<> is a collection of elements/objects. Which contains no duplicates
+        //Set<> is an interface that extends "Collection-InterFace"
+        //Set<> does NOT support indexes or positions of its elements
+
+        // HashSet<> is the implementation of Set Interface
+        // HashSet<> internally uses HashMap for its implementation
+
+        return null;
+    }
+
+}
